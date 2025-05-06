0a7ed800a4fe8e1852af0ccbc58ab7e327ca7f6a
davidvanschaik <davidvan.schaik00@gmail.com>
2025-05-06 09:24:10 +0200

    Cleaned up the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,6 @@
-// import Phaser from 'phaser';
-// import { getDirection } from "./game.js";
-// import { move } from "./snake.js";
-// import { createSnake } from "./snake.js";
-// import cursors from "./game.js";
-// import posX from "./game.js";
-// import posY from "./game.js";
-// import head from "./snake.js";
-//
-// let config = {
-//     type: Phaser.AUTO,
-//     width: 800,
-//     height: 800,
-//     scene: {
-//         preload: preload,
-//         create: create,
-//         update: update
-//     }
-// };
-//
-// let game = new Phaser.Game(config);
-//
-// function preload() {
-//     // this.load.image('food', './assets/images/food.png');
-//     this.load.spritesheet('head', './assets/images/head.png', {frameWidth: 75, frameHeight: 75});
-//     this.load.spritesheet('body', './assets/images/body.png', {frameWidth: 32, frameHeight: 32});
-// }
-//
-// function create() {
-//     for (let x = 0; x < 25; x++) {
-//         for (let y = 0; y < 25; y++) {
-//             this.add.rectangle(x * 32, y * 32, 32, 32, 0xADD8E6)
-//                 .setOrigin(0)
-//                 .setStrokeStyle(1, 0xA9A9A9);
-//         }
-//     }
-//
-//     head = this.add.sprite(posX, posY, 'head', 1);
-//     createSnake();
-//     cursors = this.input.keyboard.createCursorKeys();
-// }
-//
-// function update() {
-//     getDirection()
-//     move();
-// }
+import { preload, create, update } from './game.js';
 
-let config = {
+export let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 800,
--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,91 +1,159 @@
-// let snake = [
-//     { head: '', posX: 16, posY: 496, direction: 1, targetDirection: -1 },
-//     { body: '', posX: 16, posY: 528, direction: 1, targetDirection: -1 },
-//     { body: '', posX: 16, posY: 560, direction: 1, targetDirection: -1 },
-// ];
-import { addSpeed } from "./game.js";
+import {
+    apple,
+    scene,
+    speed,
+    cursors,
+    spawnApple,
+    getDistance,
+    addSpeed,
+    addScore,
+    setSpeed
+} from './game.js';
 
-import direction from "./game.js";
-import targetDirection from "./game.js";
-import posY from "./game.js";
-import posX from "./game.js";
+export let targetDirection = -1;
 
-let head;
-export default {head};
+export let turnPoints = [];
+let appleIsActive = true;
 
-let body1;
-let body2;
+export let snake = [
+    { body: '', direction: 1, frozen: false, index: 0, side: 'left'  }
+];
 
-function move() {
-    if (direction !== targetDirection && targetDirection !== -1) {
-        if (
-            (direction % 2 === 0 && (head.x + 16) % 32 === 0) ||
-            (direction % 2 !== 0 && (head.y + 16) % 32 === 0)
-        ) {
-            direction = targetDirection;
-            targetDirection = -1;
-            turn(direction);
+export function createSnake() {
+    snake[0].body = scene.add.sprite(48, 496, 'head', 1);
+    snake[0].body.setScale(0.425);
+
+    for (let i = 1; i < 3; i++) {
+        createBody(snake[0], snake[0].body.x, snake[0].body.y + (i * 32), false);
+    }
+}
+
+export function createBody(body, bodyX, bodyY, frozen) {
+    let newBody = {
+        body: scene.add.sprite(bodyX, bodyY, 'body', 1),
+        direction: body.direction,
+        frozen: frozen,
+        index: 0,
+        side: ''
+
+    };
+    newBody.body.setScale(0.40);
+    snake.push(newBody);
+}
+
+export function getDirection() {
+    if (snake[0].direction % 2 !== 0) {
+        if (cursors.left.isDown) {
+            targetDirection = 0;
+        }
+        if (cursors.right.isDown) {
+            targetDirection = 2;
+        }
+    } else {
+        if (cursors.up.isDown) {
+            targetDirection = 1;
+        } else if (cursors.down.isDown) {
+            targetDirection = 3;
         }
     }
-    addSpeed();
-}
-export {move};
-
-function turn(dir) {
-    console.log(head.x);
-    head.setFrame(dir);
 }
 
-function createSnake() {
-    body1 = createBody(1);
-    body2 = createBody(2);
-
-    scale(head);
-    scale(body1);
-    scale(body2);
-}
-export {createSnake};
-
-function createBody(bodyNumber) {
-    posX += 32 * bodyNumber;
-    posY += 32 * bodyNumber;
-
-    return this.add.sprite(posX, posY, 'body', 1);
-}
-
-function scale(element) {
-    element.setScale(0.45);
+export function move() {
+    snake.forEach((part, index) => {
+        if (index === 0) {
+            turnHead(part);
+        } else {
+            if (part.frozen) {
+                moveNewBody(part, index);
+            }
+            followTurnPoints(part, index);
+        }
+        if (! part.frozen) {
+            addSpeed(part);
+        }
+    });
+    setSpeed();
 }
 
-// function followTurnPoints(index, part) {
-//     for (let i = 0; i < turnPoints.length; i++) {
-//         let turn = turnPoints[i];
-//
-//         let moveX = part.body.x;
-//         let moveY = part.body.y;
-//
-//         switch (part.direction) {
-//             case 0:
-//                 moveY -= 32;
-//                 break;
-//             case 1:
-//                 moveX -= 32;
-//                 break;
-//             case 2:
-//                 moveY += 32;
-//                 break;
-//             case 3:
-//                 moveX += 32;
-//                 break;
-//         }
-//
-//         if (turn.direction % 2 !== 0 && moveX === turn.x || turn.direction % 2 === 0 && moveY === turn.y) {
-//             part.direction = turn.direction;
-//
-//             if (index === snake.length - 1) {
-//                 turnPoints.splice(i, 1);
-//                 targetDirection = -1;
-//             }
-//         }
-//     }
-// }
+function turnHead(part) {
+    let alignX = (part.body.x + 16) % 32;
+    let alignY = (part.body.y + 16) % 32;
+
+    if (targetDirection !== -1 && targetDirection !== part.direction) {
+        if (alignX === 0 && alignY === 0) {
+            makeTurn(part);
+        } else {
+            if (targetDirection % 2 !== 0 && alignX < speed && alignY === 0) {
+                part.body.x -= alignX;
+                makeTurn(part);
+            }
+            if (targetDirection % 2 === 0 && alignX === 0 && alignY < speed) {
+                part.body.y -= alignY;
+                makeTurn(part);
+            }
+        }
+    }
+}
+
+function makeTurn(part) {
+    turnPoints.push({
+        x: part.body.x,
+        y: part.body.y,
+        direction: targetDirection
+    });
+    part.direction = targetDirection;
+    part.body.setFrame(targetDirection);
+}
+
+function moveNewBody(part, index) {
+    let prevPart = snake[index -1];
+    let distance = getDistance(part.body, prevPart.body);
+
+    if (distance > 23 && part.direction !== prevPart.direction) {
+        part.frozen = false;
+    } else if (distance > 32) {
+        part.frozen = false;
+    }
+    centerGrid(part);
+}
+
+export function centerGrid(part) {
+    if (part.direction % 2 === 0) {
+        part.body.x = Math.round(part.body.x / 16) * 16;
+    } else {
+        part.body.y = Math.round(part.body.y / 16) * 16;
+    }
+}
+
+function followTurnPoints(part, index) {
+    for (let i = 0; i < turnPoints.length; i++) {
+        let turn = turnPoints[i];
+
+        if (getDistance(part.body, turn) < speed) {
+            part.body.x = turn.x;
+            part.body.y = turn.y;
+            part.direction = turn.direction;
+
+            if (index === snake.length - 1) {
+                turnPoints.splice(i, 1);
+                targetDirection = -1;
+            }
+        }
+    }
+}
+
+export function eatApple() {
+    let distance = getDistance(snake[0].body, apple);
+    let lastBody = snake[snake.length -1];
+
+    if (distance < 32 && appleIsActive) {
+        createBody(lastBody, lastBody.body.x, lastBody.body.y, true);
+        appleIsActive = false;
+    }
+    if (distance < 16 && ! appleIsActive) {
+        apple.destroy();
+        addScore();
+        spawnApple();
+        appleIsActive = true;
+    }
+}
